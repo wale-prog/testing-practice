701439787c7187c5fe8917f31b36d30a46fbfe2d
Olawale <olapetanwale@outlook.com>
2022-06-08 13:10:23 +0100

    Task1 solved

---
+++ stringLength.js
@@ -0,0 +1,8 @@
+const stringLength = (string)=>{
+if (string.length < 1 || string.length > 10){
+    throw new Error('The string length cannot be more than 10 or less than 1')
+} else{
+    return string.length;
+    }
+}
+module.exports = stringLength;
